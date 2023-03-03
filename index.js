25e60d0dd4c8acdf7ebcecc85cfb20fdfe3464ab
Brian Barnes Jr <houdinii1984@gmail.com>
2023-03-03 16:38:47 -0700

    Add no match logic/image

--- index.js
+++ index.js
@@ -5,17 +5,34 @@ const profileSection = document.getElementById("profile-section")
 const likeBtn = document.getElementById("like-btn")
 const nopeBtn = document.getElementById("nope-btn")
 
-const getNewDog = () => new Dog(data.shift())
+const getNewDog = () => {
+  const newDog = new Dog(data.shift())
+  const noDog = new Dog({
+    name: null,
+    avatar: "",
+    age: 0,
+    bio: "0",
+    hasBeenSwiped: false,
+    hasBeenLiked: false
+  })
+  return newDog ? newDog : noDog
+}
 const render = () => {
-  profileSection.innerHTML = currentDog.getProfileHtml()
-  const badge = document.getElementById("badge")
+  if(currentDog.name != null) {
+    profileSection.innerHTML = currentDog.getProfileHtml()
+    const badge = document.getElementById("badge")
 
-  if(currentDog.hasBeenSwiped){
-    badge.classList.contains('hidden')
-        ? badge.classList.remove('hidden')
-        : null
+    if (currentDog.hasBeenSwiped) {
+      badge.classList.contains('hidden')
+          ? badge.classList.remove('hidden')
+          : null
+    } else {
+      badge.classList.add('hidden')
+    }
   } else {
-    badge.classList.add('hidden')
+    document.getElementById("swipe-btns").style.display = "none"
+    profileSection.style.backgroundImage = `url('../images/no-match.png')`
+    profileSection.innerHTML = ``
   }
 }
 
